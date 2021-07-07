8e4f5c4e8f7bcf1b1bdfc8dd6a2f5201cc57f4c9
Tin Prvcic <tin.prvcic@gmail.com>
2021-07-07 11:35:47 +0200

    update main app functionality

--- App.tsx
+++ App.tsx
@@ -1,115 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
+import HomeScreen from './components/HomeScreen';
+import {createStackNavigator} from '@react-navigation/stack';
+import InfoScreen from './components/InfoScreen';
+import {Pressable, StatusBar} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import AboutScreen from './components/AboutScreen';
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
+const Stack = createStackNavigator();
 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    card: '#f04',
+    text: '#fcfcfc',
+  },
+};
 
- const Section: React.FC<{
-   title: string;
- }> = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
+const App = () => {
+  StatusBar.setBackgroundColor('#f04', false);
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={({navigation}) => {
+            return {
+              headerRight: () => (
+                <Pressable
+                  android_ripple={{color: '#ccc', borderless: true}}
+                  onPress={() => navigation.navigate('About')}>
+                  <Icon
+                    name="info"
+                    color="#fff"
+                    style={{fontSize: 24, paddingRight: 16}}
+                  />
+                </Pressable>
+              ),
+            };
+          }}
+        />
+        <Stack.Screen
+          name="Info"
+          component={InfoScreen}
+          //@ts-ignore
+          options={({route}) => ({title: route.params.name})}
+        />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
-
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
-
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
-
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
-
- export default App;
+export default App;
